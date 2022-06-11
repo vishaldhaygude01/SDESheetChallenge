68fd910dd00b7521cf2f06ef2d37129f548ea480
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2022-06-11 13:42:02 +0530

    Linked-List

---
+++ Day6_Linked_List/delete_node_linked_list.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node->next;
+        node->val = temp->val;
+        node->next = temp->next;
+    
+        delete temp;
+    }
+};
