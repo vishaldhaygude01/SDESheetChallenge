78cc099fd81c0c49d6e854ace92fef5001ee679f
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2022-06-27 21:29:54 +0530

    Stack

---
+++ Day22_Stack/next_smaller_element.cpp
@@ -0,0 +1,14 @@
+vector<int> nextSmallerElement(vector<int> &arr, int n){
+    // Write your code here.
+    vector<int> ans(n);
+    stack<int> st;
+    st.push(-1);
+    for(int i=n-1;i>=0;i--){ 
+        while(st.top() >= arr[i]){
+            st.pop();
+        }
+        ans[i] = st.top();
+        st.push(arr[i]);
+    }
+    return ans;
+}
