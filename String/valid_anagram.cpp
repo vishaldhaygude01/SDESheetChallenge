0a3c5b949b86ed3028cd5a818ef735b322a15d80
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2022-07-04 23:46:38 +0530

    String

--- String/valid_anagram.cpp
+++ String/valid_anagram.cpp
@@ -4,13 +4,16 @@ public:
     bool isAnagram(string s, string t) {
         int count [noOfChars] = {0};
         int i;
+        
         for(i=0;s[i] && t[i];i++){
             count[s[i]]++;
             count[t[i]]--;
         }
+
         if(s[i] || t[i]){
             return false;
         }
+
         for(int i=0;i<noOfChars;i++){
             if(count[i]){
                 return false;
