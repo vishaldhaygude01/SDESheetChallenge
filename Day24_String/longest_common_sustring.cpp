65a7b6efe8bac67b7dc9d8e18019178ac1f82c0b
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2022-07-20 12:13:22 +0530

    String

---
+++ Day24_String/longest_common_sustring.cpp
@@ -0,0 +1,46 @@
+class Solution{
+    public:
+    
+    int longestCommonSubstring (string s, string t, int n, int m){
+        // your code here
+        vector<int>prev(m+1,0),cur(m+1,0);
+    
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    cur[j] = 1 + prev[j-1];
+                    ans = max(ans,cur[j]);
+                }
+                else cur[j] = 0;
+            }
+            prev = cur;
+        }
+        return ans;
+    }
+};
+
+
+/*
+    //TABULATION
+    
+    int longestCommonSubstr (string s, string t, int n, int m){
+        // your code here
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int j=0;j<=m;j++) dp[0][j] = 0;
+        for(int i=0;i<=n;i++) dp[i][0] = 0;
+        
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    ans = max(ans,dp[i][j]);
+                }
+                else dp[i][j] = 0;
+            }
+        }
+        return ans;
+    }
+
+*/
